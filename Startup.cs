a985b1187db8866244cebe7850931bdd0ab3d9ad
Ahmad Zarour <zarour.ahmad@gmail.com>
2022-01-11 13:59:05 +0100

    startup updated

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,10 @@
+//using MVC_Webshop.Data;
+using MVC_Webshop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +12,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+using Rotativa.AspNetCore;
 
 namespace MVC_Webshop
 {
@@ -23,7 +32,28 @@ namespace MVC_Webshop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register the services 
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+            services.AddJsEngineSwitcher(
+                option => option.DefaultEngineName = V8JsEngine.EngineName).AddV8();
             services.AddControllersWithViews();
+            services.AddMvc().AddSessionStateTempDataProvider();
+            services.AddSession();
+            services.AddRazorPages();
+            
+
+            //dependency injection  
+            // dependencies which will be used later with Identity and DbContext
+            /*
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddDefaultUI().AddDefaultTokenProviders().AddEntityFrameworkStores<ApplicationDbContext>();
+            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,11 +69,21 @@ namespace MVC_Webshop
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            /// to be used later with React
+            ///more information on  https://reactjs.net/tutorials/aspnetcore.html 
+            /* 
+            app.UseReact(config =>
+            {
+                 config.AddScript("file");
+            });
+            */
+
+            // inject middleware in HTTP pipeline
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,7 +91,9 @@ namespace MVC_Webshop
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
+            RotativaConfiguration.Setup(env.WebRootPath, "Rotativa");
         }
     }
 }
