56cff6cbf92780b4540e6a6d69ce1bf1bc953245
Martin Johansson <marjoh3@live.se>
2022-01-20 13:57:10 +0100

    adding admin pages

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Webshop.Models;
+using MVC_Webshop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Webshop.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly BookStoreDbContext _context;
+        public AdminController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var books =_context.Books.ToList();
+            return View(books);
+        }
+        public IActionResult Books()
+        {
+
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+        public IActionResult Bookdetails(int id)
+        {
+            
+            return View(FilledBook(id));
+        }
+        private Book FilledBook(int id)
+        {
+            var books = _context.Books.ToList();
+            var book = books.First(x => x.Id == id);
+            book.Publisher = _context.Publishers.ToList().First(x => x.Id == book.PublisherId);
+            book.Genre = _context.Genres.ToList().First(x => x.Id == book.GenreId);
+            book.BookAuthors = _context.BookAuthors.ToList().FindAll(x => x.BookId == id);
+            for (int i = 0; i < book.BookAuthors.Count(); i++)
+            {
+                book.BookAuthors[i].Author = _context.Authors.ToList().First(x => x.Id == book.BookAuthors[i].AuthorId);
+            }
+            return book;
+        }
+        public IActionResult Bookedit(int id)
+        {
+            return View(FilledBook(id));
+        }
+        public IActionResult Bookcreate()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/BookDataViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MVC_Webshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Webshop.ViewModels
+{
+    public class BookDataViewModel
+    {
+        public Book Book { get; set; }
+        public Publisher Publisher { get; set; }
+        public Genre Genre { get; set; }
+        public Author Author { get; set; }
+        public BookAuthor BookAuthor { get; set; }
+    }
+}
